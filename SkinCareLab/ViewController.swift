b2695a189f52aee4769fbe14e0d85fcd4919e90c
Bruno Magalhães <magalhaesbrunop@gmail.com>
2019-01-16 11:22:41 -0200

    Initial Commit

---
+++ SkinCareLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkinCareLab
+//
+//  Created by Bruno Magalhães on 16/01/19.
+//  Copyright © 2019 Cybernetic Company of Milky Way. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
